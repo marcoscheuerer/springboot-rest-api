5026f5f4be65d454e478260aef9d3e9d8b4daab8
Marco Scheuerer <MarcoScheuerer1978@gmail.com>
2023-08-05 11:34:47 +0200

    Get-Method for a student List.

--- src/main/java/mao/linatrix/springboot/controller/StudentController.java
+++ src/main/java/mao/linatrix/springboot/controller/StudentController.java
@@ -4,6 +4,9 @@ import mao.linatrix.springboot.bean.Student;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class StudentController {
 
@@ -11,9 +14,22 @@ public class StudentController {
     // http://localhost:8181/student
     @GetMapping("student")
     public Student getStudent() {
-        Student student = new Student(1, "Ramshe", "Fadatare");
+        Student student = new Student(1, "Mampfred", "Essich");
 
         return student;
+    }
+
+    // http://localhost:8181/students
+    @GetMapping("students")
+    public List<Student> getStudents() {
+        List<Student> students = new ArrayList<>();
+        students.add(new Student(1, "Lassmi", "Randa"));
+        students.add(new Student(2, "Dennsie", "Willja"));
+        students.add(new Student(3, "Steve", "Earkel"));
+        students.add(new Student(4, "Sandra", "Bullseye"));
+        students.add(new Student(5, "Hulk", "Hogan"));
+
+        return students;
     }
 
 }

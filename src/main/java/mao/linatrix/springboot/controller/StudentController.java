4029e31b31804f1fb41fa8aee30c779d9d8d0b55
Marco Scheuerer <MarcoScheuerer1978@gmail.com>
2023-08-05 18:13:51 +0200

    Response-Entity for all Request-Methods implemented.

--- src/main/java/mao/linatrix/springboot/controller/StudentController.java
+++ src/main/java/mao/linatrix/springboot/controller/StudentController.java
@@ -3,8 +3,10 @@ package mao.linatrix.springboot.controller;
 import jakarta.annotation.PostConstruct;
 import mao.linatrix.springboot.bean.Student;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,7 +17,7 @@ public class StudentController {
 
     @GetMapping("clear_list")
     @PostConstruct
-    public String loadData() {
+    public ResponseEntity<String> loadData() {
         students = new ArrayList<>();
         students.add(new Student(1, "Lassmi", "Randa"));
         students.add(new Student(2, "Dennsie", "Willja"));
@@ -23,54 +25,61 @@ public class StudentController {
         students.add(new Student(4, "Sandra", "Bullseye"));
         students.add(new Student(5, "Hulk", "Hogan"));
 
-        return "List reloaded!";
+        //return new ResponseEntity<>("List reloaded!", HttpStatus.OK);
+        return ResponseEntity.ok().header("custom-header", "reloaded").body("List reloaded!");
     }
 
     // HTTP GET Request
     // http://localhost:8181/student
     @GetMapping("student")
-    public Student getStudent() {
+    public ResponseEntity<Student> getStudent() {
         Student student = new Student(1, "Mampfred", "Essich");
 
-        return student;
+        //return new ResponseEntity<>(student, HttpStatus.OK);
+        return ResponseEntity.ok(student);
     }
 
     // http://localhost:8181/students
     @GetMapping("students")
-    public List<Student> getStudents() {
-        return students;
+    public ResponseEntity<List<Student>> getStudents() {
+        //return new ResponseEntity<>(students, HttpStatus.OK);
+        return ResponseEntity.ok(students);
     }
 
     // Spring BOOT REST API with Path Variable
     // {id} - URI template variable
     // http://localhost:8181/students/1
     @GetMapping("students/{id}")
-    public Student getStudentById(@PathVariable("id") int studentId) {
-        return new Student(studentId, "Marco", "Engel");
+    public ResponseEntity<Student> getStudentById(@PathVariable("id") int studentId) {
+        //return new ResponseEntity<>(new Student(studentId, "Marco", "Engel"), HttpStatus.OK);
+        return ResponseEntity.ok(new Student(studentId, "Marco", "Engel"));
     }
 
     // http://localhost:8181/students/666/Marco/Teufel
     @GetMapping("students/{studentId}/{firstName}/{lastName}")
-    public Student getStudentById(@PathVariable int studentId,
+    public ResponseEntity<Student> getStudentById(@PathVariable int studentId,
                                   @PathVariable("firstName") String studentFirstName,
                                   @PathVariable("lastName") String studentLastName) {
-        return new Student(studentId, studentFirstName, studentLastName);
+        //return new ResponseEntity<>(new Student(studentId, studentFirstName, studentLastName), HttpStatus.OK);
+        Student student = new Student(studentId, studentFirstName, studentLastName);
+        return ResponseEntity.ok(student);
     }
 
     // Spring BOOT REST API with Request Param
     // http://localhost:8181/students/query1?id=1
     @GetMapping("students/query1")
-    public Student getStudentByIdParam(@RequestParam("id") int studentId) {
-        //return new Student(studentId, "Marco", "Scheuerer");
-        return students.get(studentId);
+    public ResponseEntity<Student> getStudentByIdParam(@RequestParam("id") int studentId) {
+        //return new ResponseEntity<>(students.get(studentId), HttpStatus.OK);
+        return ResponseEntity.ok(students.get(studentId));
     }
 
     // http://localhost:8181/students/query2?id=2&fname=Lena&lname=Engel
     @GetMapping("students/query2")
-    public Student getStudentByAllParam(@RequestParam("id") int studentId,
+    public ResponseEntity<Student> getStudentByAllParam(@RequestParam("id") int studentId,
                                         @RequestParam("fname") String studentFirstName,
                                         @RequestParam("lname") String studentLastName) {
-        return new Student(studentId, studentFirstName, studentLastName);
+        //return new ResponseEntity<>(new Student(studentId, studentFirstName, studentLastName), HttpStatus.OK);
+        return ResponseEntity.ok(new Student(studentId, studentFirstName, studentLastName));
     }
 
     // Spring Boot REST API that handles HTTP POST Request - creating new resource
@@ -79,19 +88,18 @@ public class StudentController {
     // @RequestBody (responsible for retrieving the HTTP request body and
     //               automatically converting it (e.g. JSON) to the Java object)
     @PostMapping("students/create")
-    @ResponseStatus(HttpStatus.CREATED)
-    public Student createStudent(@RequestBody Student student) {
+    public ResponseEntity<Student> createStudent(@RequestBody Student student) {
         System.out.println("Student ID       : " + student.getId());
         System.out.println("Student Firstname: " + student.getFirstName());
         System.out.println("Student Lastname : " + student.getLastName());
 
-        return student;
+        //return new ResponseEntity<>(student, HttpStatus.CREATED);
+        return ResponseEntity.created(URI.create("students/create")).build();
     }
 
     // Spring Boot REST API that handles HTTP PUT Request - updating existing resource
     @PutMapping("students/{id}/update")
-    @ResponseStatus(HttpStatus.CREATED)
-    public List<Student> updateStudent(@RequestBody Student student, @PathVariable("id") int studentId) {
+    public ResponseEntity<List<Student>> updateStudent(@RequestBody Student student, @PathVariable("id") int studentId) {
         student.setId(studentId);
 
         students.set(studentId - 1, student);
@@ -99,19 +107,23 @@ public class StudentController {
         System.out.println(student.getFirstName());
         System.out.println(student.getLastName());
 
-        return students;
+        //return new ResponseEntity<>(students, HttpStatus.CREATED);
+        return ResponseEntity.created(URI.create(String.format("students/%d/update", studentId))).build();
     }
 
     // Spring Boot REST API that handles HTTP DELETE Request - deleting the existing resource
     @DeleteMapping("students/{id}/delete")
-    public String deleteStudent(@PathVariable("id") int studentId) {
+    public ResponseEntity<String> deleteStudent(@PathVariable("id") int studentId) {
         int id = students.get(studentId - 1).getId();
         String lastName = students.get(studentId - 1).getLastName();
         String firstName = students.get(studentId -1).getFirstName();
 
         students.remove(studentId - 1);
 
-        return "Student at List Pos. " + studentId + " (ID " + id + ": " + lastName + ", " + firstName + ") deleted successfully";
+        String result = "Student at List Pos. " + studentId + " (ID " + id + ": " + lastName + ", " + firstName + ") deleted successfully";
+
+        //return new ResponseEntity<>(result, HttpStatus.OK);
+        return ResponseEntity.ok(result);
     }
 
 }
